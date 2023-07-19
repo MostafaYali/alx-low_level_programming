1997fc81ae7b6c4f346d6ffe5aacd2ef89389577
mostafa yosuri ali <myali490@gmail.com>
2023-07-18 18:11:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: Prints whether a random number is positive or negative
+ * Description: Prints "_putchar" followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (!n)
-		printf("%d is zero\n", n);
-	else
-		(n > 0) ? printf("%d is positive\n", n) : printf("%d is negative\n", n);
-
+	write(STDOUT_FILENO, "_putchar\n", 9);
 	return (0);
 }
